09f203362e7cfa85339f0aabc35a6e3b59f51093
chytran <chytran@users.noreply.github.com>
2021-08-02 13:29:23 -0700

    Working on php side of registration

--- dating-app/includes/signup.inc.php
+++ dating-app/includes/signup.inc.php
@@ -2,4 +2,15 @@
 
 if (isset($_POST["submit"])) {
     
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    require_once 'database.inc.php';
+    require_once 'functions.inc.php';
+
+    if (emptyInputSignup($name, $email, $password) !== false) {
+        header("location: ../Registration.php?error=emptyinput");
+        exit();
+    }
 }
