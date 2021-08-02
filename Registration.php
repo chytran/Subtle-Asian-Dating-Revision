ed20b5e195308b597b6d97126f98e5fcf4cdb4bb
chytran <chytran@users.noreply.github.com>
2021-08-02 12:58:35 -0700

    Registration design completed

--- Registration.php
+++ Registration.php
@@ -21,30 +21,33 @@
     <main class="main-1">
         <section class="hero">
             <div class="hero__container">
-                <div class="form__content">
-                    <div class="info__container">
-                        <h1>Sign Up</h1>
-                        <form action="dating-app/includes/signup.inc.php" class="form">
-                            <div class="first__name__container">
-                                <spam class="first__name__title">Name</spam>
-                                <input type="text" name="name"></input>
-                            </div>
-                            <div class="email__container">
-                                <spam class="email__title">Email</spam>
-                                <input type="text" name="email"></input>
-                            </div>
-                            <div class="password__container">
-                                <spam class="password__title">Password</spam>
-                                <input type="text" name="password"></input>
-                            </div>
+                <div class="form__content__signup">
+                    <h1 class="signup__header">Sign Up</h1>
+                    <form action="dating-app/includes/signup.inc.php" class="form">
+                        <div class="name__container">
+                            <spam class="name__title">Name</spam>
+                            <input type="text" name="name" class="input"></input>
+                        </div>
+                        <div class="email__container">
+                            <spam class="email__title">Email</spam>
+                            <input type="text" name="email" class="input"></input>
+                        </div>
+                        <div class="password__container">
+                            <spam class="password__title">Password</spam>
+                            <input type="text" name="password" class="input"></input>
+                        </div>
+                        <div class="radio__container">
                             <input type="radio" class="accept">
-                            <button type="submit" name="submit">Sign Up</button>
-                        </form> 
-            
-                    </div>
+                            <span class="term__of__use">I accept the terms of use</span>
+                        </div>
+                        <button type="submit" name="submit" class="signup__button">Sign Up</button>
+                    </form> 
                 </div>
             </div>
         </section>
     </main>
+    <script>
+        $(document).ready
+    </script>
 </body>
 </html>
