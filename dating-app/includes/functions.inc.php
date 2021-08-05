e925e5e8cc9636ebf4ef12fd2b8b9b8e44ebdcc4
chytran <chytran@users.noreply.github.com>
2021-08-05 13:33:26 -0700

    Adding more functions

--- dating-app/includes/functions.inc.php
+++ dating-app/includes/functions.inc.php
@@ -23,7 +23,7 @@ function invalidEmail($email) {
 }
 
 function uidExists($conn, $name, $email) {
-    $sql = "SELECT * FROM users WHERE usersName = ? or usersEmail = ?;";
+    $sql = "SELECT * FROM users WHERE userName = ? or userEmail = ?;";
     $stmt = mysqli_stmt_init($conn);
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); 
     if (!mysqli_stmt_prepare($stmt, $sql)) {
@@ -45,4 +45,32 @@ function uidExists($conn, $name, $email) {
     }
 
     mysqli_stmt_close($stmt);
+}
+
+function createUser($conn, $name, $email, $pwd) {
+    $sql = "INSERT INTO users (userName, userEmail, userPassword");
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../../Registration.php?error=stmtfailed");
+        exit();
+    }
+
+    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+    mysqli_stmt_bind_param($stmt, "sss", $name, $email, $hashedPwd);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    header("location: ../../Registration.php?error=none");
+    exit();
+}
+
+function emptyInputLogin($email, $pwd) {
+    $result;
+    if (empty($email) || empty($pwd)) {
+        $result = true;
+    }
+    else {
+        $result = false;
+    }
+    return $result;
 }
