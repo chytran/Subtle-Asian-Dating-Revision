3e980e52e73dac6a48487db92c317a3efc71cbd4
chytran <chytran@users.noreply.github.com>
2021-08-12 10:38:36 -0700

    Continuing setting up errors

--- Registration.php
+++ Registration.php
@@ -52,6 +52,21 @@
                     if($_GET["error"] == "emptyinput") {
                         echo "<p>Fill in all fields! </p>";
                     }
+                    else if ($_GET["error"] == "invaliduid") {
+                        echo "<p>Choose a proper username</p>";
+                    }
+                    else if ($_GET["error"] == "invalidemail") {
+                        echo "<p>Choose a proper email</p>";
+                    }
+                    else if ($_GET["error"] == "stmtfailed") {
+                        echo "<p>Something went wrong, try again</p>";
+                    }
+                    else if ($_GET["error"] == "usernametaken") {
+                        echo "<p>Username already taken</p>";
+                    }
+                    else if ($_GET["error"] == "none") {
+                        echo "<p>You have signed up</p>";
+                    }
                 }
                 ?>
             </div>
